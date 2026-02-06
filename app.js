6c130dc001974cc1e5e5f488da810a026ee27225
Mohit <mohitbaghel1234321@gmail.com>
2026-02-06 23:18:14 +0530

    1 class complete

--- app.js
+++ app.js
@@ -3,18 +3,15 @@ const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
 
-
-main().catch(err => console.log(err));
+// mongosh
+main().then((res) => {
+    console.log("Connected to DB")
+})
+.catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
-
-
-
-
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
