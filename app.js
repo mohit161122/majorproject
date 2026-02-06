31d8dcb239bf6c283452dc286546d229d16470cd
Mohit <mohitbaghel1234321@gmail.com>
2026-02-07 00:07:37 +0530

    2 complete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
+const Listing = require("./models/listing.js");
+
 
 // mongosh
 main().then((res) => {
@@ -16,6 +18,20 @@ async function main() {
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/testListing', async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My new Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Caslangute , Goa",
+    country: "India", 
+  });
+ await sampleListing.save();
+ console.log("sample was saved");
+ res.send("sucessfull testing");
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
