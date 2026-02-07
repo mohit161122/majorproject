01e622d843f6ec2ef36151ab068708097994b4da
Mohit <mohitbaghel1234321@gmail.com>
2026-02-08 00:22:44 +0530

    started

--- app.js
+++ app.js
@@ -16,24 +16,32 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
+app.set("view engine" , "ejs" );
+app.set("views" , path.join(__dirname, "views"));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 
-app.get('/testlisting', async (req, res) => {
-  let sampleListings = new Listing ({
-    tital: "My new Villa",
-    description:  "by the beach"  ,
-    prices:  1200  , 
-    location:   "Calangute Goa" ,
-    country : "India",
-  });
-
-  await sampleListings.save();
-  console.log("sample was saved");
-  res.send("sacessfully testing ");
+app.get('/listings',  async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index", { allListings });
 });
+
+// app.get('/testlisting', async (req, res) => {
+//   let sampleListings = new Listing ({
+//     tital: "My new Villa",
+//     description:  "by the beach"  ,
+//     prices:  1200  , 
+//     location:   "Calangute Goa" ,
+//     country : "India",
+//   });
+
+//   await sampleListings.save();
+//   console.log("sample was saved");
+//   res.send("sacessfully testing ");
+// });
 
 
 app.listen(port, () => {
