11e242bf1a7192b2160d4a8997dfb63a1691e026
Mohit <mohitbaghel1234321@gmail.com>
2026-02-08 00:50:53 +0530

    7 complete

--- app.js
+++ app.js
@@ -18,16 +18,25 @@ async function main() {
 
 app.set("view engine" , "ejs" );
 app.set("views" , path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
+//Index
 app.get('/listings',  async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index", { allListings });
 });
+
+//show route
+app.get('/listings/:id', async (req, res) => {
+  let {id} = req.params;
+   const listing =  await Listing.findById(id);
+   res.render("listings/show.ejs" ,{listing})
+});
+
 
 // app.get('/testlisting', async (req, res) => {
 //   let sampleListings = new Listing ({
