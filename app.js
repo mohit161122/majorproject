eb1226c8358fc4966982fd65a28b5cf7d1f14a24
Mohit <mohitbaghel1234321@gmail.com>
2026-02-08 16:03:48 +0530

    Update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 8080;
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 
 // mongosh
@@ -19,6 +20,7 @@ async function main() {
 app.set("view engine" , "ejs" );
 app.set("views" , path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Game is started");
@@ -54,7 +56,20 @@ app.post("/listings", async (req, res) => {
   res.redirect("/listings");
 });
 
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let {id} = req.params;
+   const listing =  await Listing.findById(id);
+   res.render("listings/edit.ejs", { listing });
+    // res.redirect("/listings");
+});
 
+//Update rought
+app.put("/listings/:id", async (req, res) => {
+  let {id} = req.params;
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  res.redirect(`/listings/${id}`);
+});
 
 
 // app.get('/testlisting', async (req, res) => {
