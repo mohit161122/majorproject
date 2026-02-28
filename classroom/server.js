df7b695428b9a08428c33d9a6deadb72dcf9a8b6
Mohit <mohitbaghel1234321@gmail.com>
2026-02-28 18:47:39 +0530

    Phase 2 part B complete

--- classroom/server.js
+++ classroom/server.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const users = require('./routes/user.js')
-const posts= require('./routes/post.js')
+const express = require('express');
+const app = express();
+const port = 3000;
+const users = require('./routes/user.js');
+const posts= require('./routes/post.js');
+const cookieParser = require('cookie-parser');
+
+
+app.use(cookieParser("secretecode"));
+//send cookies
+app.get("/getsignedcookies" , (req,res) => {
+  res.cookie("Mande-in" , "India", {signed: true} );
+  res.send("send you some signed cookies! ");
+});
+//verify cookies
+app.get("/verify", (req,res)=> {
+  console.log(req.signedCookies);
+  res.send("varified");
+})
+
+
+
+//cookies
+app.get("/getcookies" , (req,res)=>{
+  res.cookie("greet", "namastae");
+  res.cookie("madeIn" , "India");
+  res.send("send you some cookies! ");
+});
+
+app.get("/greet" , (req , res) => {
+  let {name = "anonymous"} = req.cookies;
+  res.send(`Hi, ${name}!`);
+});
 
 
 
 app.get('/', (req, res) => {
+  console.dir(req.cookies);
   res.send('Hi, I am root!')
 });
 
@@ -17,4 +46,4 @@ app.use( "/posts" , posts);
 
 app.listen(port, () => {
   console.log(`Server is listening to  ${port}`)
-})
+});
