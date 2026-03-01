0db6b8ef7aec62e327343e8965361b7fd84a128d
Mohit <mohitbaghel1234321@gmail.com>
2026-03-01 14:14:18 +0530

    phase 2 part C

--- classroom/server.js
+++ classroom/server.js
@@ -3,47 +3,64 @@ const app = express();
 const port = 3000;
 const users = require('./routes/user.js');
 const posts= require('./routes/post.js');
-const cookieParser = require('cookie-parser');
+const session = require("express-session");
+const flash = require("connect-flash");
+const { name } = require('ejs');
+const path = require('path');
 
+app.set("view engine" , "ejs" );
+app.set("views" , path.join(__dirname, "views"));
 
-app.use(cookieParser("secretecode"));
-//send cookies
-app.get("/getsignedcookies" , (req,res) => {
-  res.cookie("Mande-in" , "India", {signed: true} );
-  res.send("send you some signed cookies! ");
-});
-//verify cookies
-app.get("/verify", (req,res)=> {
-  console.log(req.signedCookies);
-  res.send("varified");
-})
+const sessionOptions = { 
+  secret: "mysupersecretstring" ,
+   resave: false ,
+    saveUninitialized: true
+};
 
+app.use(session(sessionOptions));
+ app.use(flash());
 
-
-//cookies
-app.get("/getcookies" , (req,res)=>{
-  res.cookie("greet", "namastae");
-  res.cookie("madeIn" , "India");
-  res.send("send you some cookies! ");
-});
-
-app.get("/greet" , (req , res) => {
-  let {name = "anonymous"} = req.cookies;
-  res.send(`Hi, ${name}!`);
+app.get("/register" , (req,res) => {
+  let { name = "Anonymous"} = req.query;
+  req.session.name = name;
+  req.flash("sucess" , " user is registered successfully! ");
+  res.redirect("/hello");
 });
 
-
-
-app.get('/', (req, res) => {
-  console.dir(req.cookies);
-  res.send('Hi, I am root!')
+app.get("/hello" ,(req,res) => {
+  
+  res.render("page.ejs" , {name: req.session.name, msg: req.flash("sucess")});
 });
 
 
-app.use( "/users" , users);
-app.use( "/posts" , posts);
+
+// app.use(session({secret: "mysupersecretstring" , resave: false , saveUninitialized: true} ));
+
+
+
+
+
+// app.get ("/request" , (req,res) => {
+//   if(req.session.count){
+//     req.session.count++;
+//   }else{
+//     req.session.count = 1;
+//   }
+//   res.send(`you send a request ${req.session.count} times`); 
+// });
+
+
+
+
+
+// app.get("/test" , (req,res) => {
+//   res.send("test is successful")
+// })
+
 
 
 app.listen(port, () => {
   console.log(`Server is listening to  ${port}`)
 });
+
+
